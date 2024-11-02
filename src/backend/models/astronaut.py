2e57e8c2d9c46990bb1f66291d95ded0fa04debb
Jaya Iskandar <jayaiskandar17@gmail.com>
2024-11-02 17:18:20 -0400

    Added tables column for astronaut & iss

--- src/backend/models/astronaut.py
+++ src/backend/models/astronaut.py
@@ -8,19 +8,21 @@ class Astronaut(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=False, nullable=False)
     # water_id = db.Column(db.Integer, models.ForeignKey("app.Model"))
-    dob = db.Column(db.datetime, nullable=False)
+    # dob = db.Column(db.datetime, nullable=False)
     weight = db.Column(db.Float, nullable=False)
     height = db.Column(db.Float, nullable=False)
     age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
     
     water = db.relationship(Water, back_populates='author') #fix l8r
     
     def __repr__(self):
         return f'<Astronaut name: {self.name}>'
     
-    def __init__(self, name="John Doe", dob=datetime.date.today, weight=70, height=160, age=40):
+    def __init__(self, name="John Doe", dob=datetime.date.today(), weight=70, height=160, age=40, gender="male"):
         self.name = name
         self.dob = dob
         self.weight = weight
         self.height = height
         self.age = age
+        self.gender = gender
--- src/backend/models/iss.py
+++ src/backend/models/iss.py
@@ -8,10 +8,12 @@ class Iss(db.Model):
 
     totalWaterAmt = db.Column(db.Integer, nullable=False)
     totalAstronauts = db.Column(db.Integer, nullable=False)
-    
+    stationName = db.Column(db.String(100), nullable=False)  # Adjust length as needed
+
     def __repr__(self):
         return f'<ISS ID: {self.id}>'
     
-    def __init__(self, totalWaterAmt=900000, totalAstronauts=5):
+    def __init__(self, totalWaterAmt=900000, totalAstronauts=5, stationName="Default Station"):
         self.totalWaterAmt = totalWaterAmt
         self.totalAstronauts = totalAstronauts
+        self.stationName = stationName
